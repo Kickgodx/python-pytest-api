687f50194f8849ad5be8329d68777fd50ff0b1cb
Den <wheelman4000@gmail.com>
2025-05-03 02:02:27 +0300

    refactor: enhance logging functionality with request and response methods

--- src/tech/custom_logger.py
+++ src/tech/custom_logger.py
@@ -1,9 +1,12 @@
 import atexit
+import inspect
 import logging
 import multiprocessing
 import os
 from logging import FileHandler
 from logging.handlers import QueueHandler, QueueListener
+
+from requests import Response
 
 import config as cfg
 from src.tech.utc_log_formatter import UtcFormatter
@@ -40,6 +43,123 @@ def shutdown_logger():
     queue.close()
     queue.join_thread()
     file_log_handler.close()
+
+
+def log_request(request_id: str, method: str, url: str, **kwargs) -> None:
+    """Метод для логирования информации о запросе
+    :param request_id: уникальный идентификатор запроса
+    :param method: HTTP-метод
+    :param url: URL-адрес запроса
+    :param kwargs: дополнительные параметры запроса
+    :return: None
+    """
+    filename, lineno, funcname = get_caller_info()
+    logger.info(f"RequestID: [{request_id}] - Request: {method} {url} - {filename}:{lineno} - {funcname}")
+    if "headers" in kwargs:
+        headers_to_log = mask_bearer_tokens(kwargs["headers"])
+        logger.info(f"RequestID: [{request_id}] - Headers: {headers_to_log}")
+    if "params" in kwargs:
+        logger.info(f"RequestID: [{request_id}] - Params: {kwargs['params']}")
+    if "json" in kwargs:
+        logger.info(f"RequestID: [{request_id}] - Payload (json): {kwargs['json']}")
+    if "data" in kwargs:
+        logger.info(f"RequestID: [{request_id}] - Payload (data): {kwargs['data']}")
+
+
+def log_response(request_id: str, response: Response) -> None:
+    """Метод для логирования информации об ответе
+    :param request_id: уникальный идентификатор запроса
+    :param response: объект ответа на запрос (requests.Response)
+    :return: None
+    """
+    logger.info(f"[{request_id}] - Response: {response.status_code} {response.url}")
+    logger.info(f"[{request_id}] - Response headers: {response.headers}")
+    logger.info(f"[{request_id}] - Response body: {response.text}\n")
+
+
+def log_error(
+    request_id: str, err, response: Response | None, data, headers: dict, url: str, method: str, filename, lineno, funcname
+) -> None:
+    """Метод для логирования информации об ошибке
+    :param request_id: уникальный идентификатор запроса
+    :param err: объект ошибки (HTTPError или RequestException)
+    :param response: объект ответа на запрос (requests.Response)
+    :param data: данные запроса
+    :param headers: заголовки запроса (dict)
+    :param url: URL-адрес запроса
+    :param method: HTTP-метод
+    :param filename: имя файла, из которого был вызван запрос
+    :param lineno: номер строки, из которой был вызван запрос
+    :param funcname: имя функции, из которой был вызван запрос
+    :return: None
+    """
+    if filename is None or lineno is None or funcname is None:
+        filename, lineno, funcname = get_caller_info()
+
+    test_name = os.environ.get("PYTEST_CURRENT_TEST", "Unknown test")
+
+    log_lines = []
+
+    if test_name != "Unknown test":
+        log_lines.append(f"Test: {test_name}")
+    else:
+        log_lines.append("Unknown test")
+
+    log_lines.append(f"[{request_id}] - Error in: {filename}:{lineno} - {funcname}")
+    log_lines.append(f"[{request_id}] - {err}")
+    log_lines.append(f"[{request_id}] - Request URL: {method} {url}")
+
+    headers_to_log = mask_bearer_tokens(headers)
+    log_lines.append(f"[{request_id}] - Request headers: {headers_to_log}")
+    log_lines.append(f"[{request_id}] - Request body: {data}")
+
+    if response is not None:
+        log_lines.append(f"[{request_id}] - Response headers: {response.headers}")
+        log_lines.append(f"[{request_id}] - Response body: {response.text}\n")
+    else:
+        log_lines.append(f"[{request_id}] - Response body: None\n")
+
+    # Объединяем все строки с переносами
+    logger.error("\n".join(log_lines))
+
+
+def log_info(message: str) -> None:
+    """Метод для логирования информации
+    :param message: сообщение для логирования
+    :return: None
+    """
+    logger.info(f"{message}")
+
+
+def get_caller_info() -> tuple[str, int, str]:
+    for frame in inspect.stack():
+        module = inspect.getmodule(frame[0])
+        filename = module.__file__ if module else "Unknown"
+        if filename != __file__:
+            lineno = frame.lineno
+            funcname = frame.function
+            return filename, lineno, funcname
+    return "Unknown", 0, "Unknown"
+
+
+def mask_bearer_tokens(headers: dict) -> dict:
+    """Маскирует Bearer-токены во всех значениях словаря headers.
+    Возвращает новый словарь с замаскированными данными.
+    """
+    masked_headers = {}
+    for key, value in headers.items():
+        if isinstance(value, str) and "Bearer " in value:
+            # Разделяем строку по Bearer и маскируем токен
+            parts = value.split("Bearer ")
+            if len(parts) > 1:
+                # Оставляем 'Bearer ', но заменяем сам токен на '*****'
+                masked_value = f"{parts[0]}Bearer *****"
+                masked_headers[key] = masked_value
+            else:
+                masked_headers[key] = value
+        else:
+            masked_headers[key] = value
+    return {**headers, **masked_headers}
 
 
 atexit.register(shutdown_logger)
--- src/tech/custom_requester.py
+++ src/tech/custom_requester.py
@@ -1,6 +1,4 @@
-import inspect
 import json
-import os
 import uuid
 import warnings
 
@@ -10,7 +8,7 @@ import urllib3
 from requests import HTTPError, Response
 from urllib3.exceptions import InsecureRequestWarning
 
-from src.tech.custom_logger import logger
+from src.tech.custom_logger import get_caller_info, log_request, log_error, log_response, log_info
 
 HTTP_METHODS = ("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
 DEFAULT_TIMEOUT = 30
@@ -30,94 +28,7 @@ class CustomRequester:
 
     def close(self):
         self.session.close()
-        logger.info("Session closed")
-
-    @staticmethod
-    def _get_caller_info() -> tuple[str, int, str]:
-        for frame in inspect.stack():
-            module = inspect.getmodule(frame[0])
-            filename = module.__file__ if module else "Unknown"
-            if filename != __file__:
-                lineno = frame.lineno
-                funcname = frame.function
-                return filename, lineno, funcname
-        return "Unknown", 0, "Unknown"
-
-    def _log_request(self, request_id: str, method: str, url: str, **kwargs) -> None:
-        """Метод для логирования информации о запросе
-        :param request_id: уникальный идентификатор запроса
-        :param method: HTTP-метод
-        :param url: URL-адрес запроса
-        :param kwargs: дополнительные параметры запроса
-        :return: None
-        """
-        filename, lineno, funcname = self._get_caller_info()
-        logger.info(f"RequestID: [{request_id}] - Request: {method} {url} - {filename}:{lineno} - {funcname}")
-        if "headers" in kwargs:
-            headers_to_log = self._mask_bearer_tokens(kwargs["headers"])
-            logger.info(f"RequestID: [{request_id}] - Headers: {headers_to_log}")
-        if "params" in kwargs:
-            logger.info(f"RequestID: [{request_id}] - Params: {kwargs['params']}")
-        if "json" in kwargs:
-            logger.info(f"RequestID: [{request_id}] - Payload (json): {kwargs['json']}")
-        if "data" in kwargs:
-            logger.info(f"RequestID: [{request_id}] - Payload (data): {kwargs['data']}")
-
-    @staticmethod
-    def _log_response(request_id: str, response: Response) -> None:
-        """Метод для логирования информации об ответе
-        :param request_id: уникальный идентификатор запроса
-        :param response: объект ответа на запрос (requests.Response)
-        :return: None
-        """
-        logger.info(f"[{request_id}] - Response: {response.status_code} {response.url}")
-        logger.info(f"[{request_id}] - Response headers: {response.headers}")
-        logger.info(f"[{request_id}] - Response body: {response.text}\n")
-
-    def _log_error(
-        self, request_id: str, err, response: Response | None, data, headers: dict, url: str, method: str, filename, lineno, funcname
-    ) -> None:
-        """Метод для логирования информации об ошибке
-        :param request_id: уникальный идентификатор запроса
-        :param err: объект ошибки (HTTPError или RequestException)
-        :param response: объект ответа на запрос (requests.Response)
-        :param data: данные запроса
-        :param headers: заголовки запроса (dict)
-        :param url: URL-адрес запроса
-        :param method: HTTP-метод
-        :param filename: имя файла, из которого был вызван запрос
-        :param lineno: номер строки, из которой был вызван запрос
-        :param funcname: имя функции, из которой был вызван запрос
-        :return: None
-        """
-        if filename is None or lineno is None or funcname is None:
-            filename, lineno, funcname = self._get_caller_info()
-
-        test_name = os.environ.get("PYTEST_CURRENT_TEST", "Unknown test")
-
-        log_lines = []
-
-        if test_name != "Unknown test":
-            log_lines.append(f"Test: {test_name}")
-        else:
-            log_lines.append("Unknown test")
-
-        log_lines.append(f"[{request_id}] - Error in: {filename}:{lineno} - {funcname}")
-        log_lines.append(f"[{request_id}] - {err}")
-        log_lines.append(f"[{request_id}] - Request URL: {method} {url}")
-
-        headers_to_log = self._mask_bearer_tokens(headers)
-        log_lines.append(f"[{request_id}] - Request headers: {headers_to_log}")
-        log_lines.append(f"[{request_id}] - Request body: {data}")
-
-        if response is not None:
-            log_lines.append(f"[{request_id}] - Response headers: {response.headers}")
-            log_lines.append(f"[{request_id}] - Response body: {response.text}\n")
-        else:
-            log_lines.append(f"[{request_id}] - Response body: None\n")
-
-        # Объединяем все строки с переносами
-        logger.error("\n".join(log_lines))
+        log_info("Session closed")
 
     def _send_request(
         self, method: str, endpoint: str, data=None, headers: dict = None, params=None, use_allure: bool = True, **kwargs
@@ -128,11 +39,11 @@ class CustomRequester:
             raise ValueError(err_msg)
 
         request_id = str(uuid.uuid4())
-        filename, lineno, funcname = self._get_caller_info()
+        filename, lineno, funcname = get_caller_info()
         url = f"{self.base_url}{endpoint}"
         combined_headers = {**headers}
 
-        self._log_request(request_id, method, url, headers=combined_headers, params=params, data=data, **kwargs)
+        log_request(request_id, method, url, headers=combined_headers, params=params, data=data, **kwargs)
 
         try:
             response = self.session.request(
@@ -142,12 +53,12 @@ class CustomRequester:
             self._add_request_attachments(method, url, headers, data, params)
             exception_name = e.__class__.__name__
             err_msg = f"исключение при {method.upper()} запросе {endpoint}:\n{e}"
-            self._log_error(
+            log_error(
                 request_id, f"{exception_name} {err_msg}", None, data, combined_headers, url, method, filename, lineno, funcname
             )
             raise e.__class__(err_msg) from e
 
-        self._log_response(request_id, response)
+        log_response(request_id, response)
 
         if use_allure:
             self._add_request_attachments(method, url, response.request.headers, data, params)
@@ -158,7 +69,7 @@ class CustomRequester:
                 response.raise_for_status()
             except HTTPError as e:
                 exception_name = e.__class__.__name__
-                self._log_error(request_id, f"{exception_name}: {e}", response, data, combined_headers, url, method, None, None, None)
+                log_error(request_id, f"{exception_name}: {e}", response, data, combined_headers, url, method, None, None, None)
 
         return response
 
@@ -210,23 +121,3 @@ class CustomRequester:
 
         if params:
             allure.attach(name="Request params", body=json.dumps(params, indent=2), attachment_type=allure.attachment_type.JSON)
-
-    @staticmethod
-    def _mask_bearer_tokens(headers: dict) -> dict:
-        """Маскирует Bearer-токены во всех значениях словаря headers.
-        Возвращает новый словарь с замаскированными данными.
-        """
-        masked_headers = {}
-        for key, value in headers.items():
-            if isinstance(value, str) and "Bearer " in value:
-                # Разделяем строку по Bearer и маскируем токен
-                parts = value.split("Bearer ")
-                if len(parts) > 1:
-                    # Оставляем 'Bearer ', но заменяем сам токен на '*****'
-                    masked_value = f"{parts[0]}Bearer *****"
-                    masked_headers[key] = masked_value
-                else:
-                    masked_headers[key] = value
-            else:
-                masked_headers[key] = value
-        return {**headers, **masked_headers}
